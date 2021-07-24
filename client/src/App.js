d524c3a4d09a01b1b398803f342cc1710cf6c65d
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-23 18:23:56 -0700

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import Header from './components/Header';
 import Home from './components/Home';
 import './styles/App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-        <Header/>
-        <Home/>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <Header/>
+          </Route>
+          <Route path='/'>
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
