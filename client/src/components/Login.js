f67dc4cf147edb175567c1c1fcdc7befa630f206
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-26 18:30:31 -0700

    Login page set up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <div>
-            
+        <div className='login'>
+            <Link to='/'>
+                <img 
+                    className='login__logo'
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/175px-Amazon_logo.svg.png' 
+                />
+            </Link>
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input type='text' data-lpignore='true'/>
+                    <h5>Password</h5>
+                    <input type='password' data-lpignore='true'/>
+                    <button className='login__signin__button'>Continue</button>
+                </form>
+                <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+                <button className='login__register__button'>Create your Amazon account</button>
+            </div>
         </div>
     )
 }
