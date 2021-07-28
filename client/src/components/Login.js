3632290659a27bd1101a7fd582de48c6baddd482
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-27 17:08:11 -0700

    changes to login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,6 +6,15 @@ const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const signIn = (e) => {
+        e.preventDefault()
+
+    }
+
+    const register = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -18,13 +27,30 @@ const Login = () => {
                 <h1>Sign-in</h1>
                 <form>
                     <h5>Email</h5>
-                    <input type='text' data-lpignore='true'/>
+                    <input 
+                        type='text' 
+                        data-lpignore='true' 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     <h5>Password</h5>
-                    <input type='password' data-lpignore='true'/>
-                    <button className='login__signin__button'>Continue</button>
+                    <input 
+                        type='password' 
+                        data-lpignore='true' 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button 
+                        className='login__signin__button'
+                        type='submit'
+                        onClick={signIn}
+                    >Continue</button>
                 </form>
                 <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
-                <button className='login__register__button'>Create your Amazon account</button>
+                <button 
+                    className='login__register__button'
+                    onClick={register}
+                >Create your Amazon account</button>
             </div>
         </div>
     )
