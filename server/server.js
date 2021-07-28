bc1d968b17928b8fc83947ee499a16e8954e16c7
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-28 08:31:29 -0700

    server set-up

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const connectionURL = 'mongodb://localhost/amazon-clone'
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+let db = mongoose.connection
+
+// check db connection
+db.once('open', () => {
+    console.log(`connected to ${connectionURL}`)
+})
+
+// check for db errors
+db.on('error', () => {
+    console.log(err)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send('amazon-clone')
+})
+
+app.listen(port, () => {
+    console.log(`listening on localhost ${port}`)
+})
