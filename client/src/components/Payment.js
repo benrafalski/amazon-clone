cf08d628eaf0c7b27fa85eebfa7e2e70f2ed8cc9
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-29 08:09:06 -0700

    Payment page styled

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,9 +1,55 @@
+import { Link } from '@material-ui/core'
+import { useStateValue } from '../StateProvider'
 import '../styles/Payment.css'
+import CheckoutProduct from './CheckoutProduct'
 
 const Payment = () => {
+    const [{ cart, user }, dispatch] = useStateValue()
+
     return (
         <div className='payment'>
-            
+            <div className='payment__container'>
+                <h1>
+                    Checkout 
+                    (<Link to='/checkout' style={{ textDecoration:'none' }}>
+                        {cart?.length} items    
+                    </Link>)
+                </h1>
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className='payment__address'>
+                        <p>{user?.email}</p>
+                        <p>123 react lane</p>
+                        <p>Los Angeles, CA</p>
+                    </div>
+                </div>
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Review Items and Delivery</h3>
+                    </div>
+                    <div className='payment__items'>
+                        {cart.map((item) => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className='payment__details'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
